990a7ddd00820028cae82223498c73ca1d70b38e
romansharapov19 <r.sharapov@kigroup.de>
2023-06-02 16:58:40 +0100

    install langchain and start comunication with the ai chat

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
+const { ChatOpenAI } = require("langchain/chat_models/openai");
+const {HumanChatMessage} = require("langchain/schema");
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
@@ -9,6 +12,7 @@ app.use(express.json());
 
 const rooms = [];
 const messagesByRoom = {};
+const model = new ChatOpenAI({openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.9});
 
 app.post('/rooms', (req, res) => {
   const room = req.body.room;
@@ -28,6 +32,21 @@ app.get('/messages/:room', (req, res) => {
   const room = req.params.room;
   res.json(messagesByRoom[room] ?? []);
 });
+
+app.get('/chat', async () => {
+  //First try, just an example of usage
+  try {
+    const res = await model.call([
+      new HumanChatMessage("What would be a good company name a company that makes colorful socks?")
+    ]);
+    console.log(res);
+
+  }catch (e){
+    console.log('Error: ',e)
+  }
+
+});
+
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -58,6 +77,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3001, () => {
+server.listen(3001, async () => {
   console.log('Server listening on port 3001');
 });
